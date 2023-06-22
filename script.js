7334b91122c09f3ffd4b0160ad7e752a944ec38f
Ashif <46344066+Ashifcoder@users.noreply.github.com>
2023-06-22 18:48:35 +0530

    Files Added

---
+++ script.js
@@ -0,0 +1,69 @@
+var words = [ "Researcher","Pentester", "Speaker" ]
+var counter = 0;
+var currentIndex = getRandomInt(0,words.length - 1);
+var text = document.querySelector("#type-it")
+var stepInterval = setInterval(() => {step();}, 200);
+var delInterval = null;
+var delTimeout = null;
+
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;   
+}
+
+function delIntervalCallback() {
+    delInterval = setInterval(() => {
+        del();
+    }, 100)
+    clearTimeout(delTimeout);
+}
+
+function del () {
+    if (counter == 0){
+        let newIndex =  getRandomInt(0,words.length - 1);
+        while (newIndex == currentIndex){
+            newIndex =  getRandomInt(0,words.length - 1);
+        }
+        currentIndex = newIndex;
+        clearInterval(delInterval);
+        stepInterval = setInterval (() => {step();}, 200);
+    }
+    else {
+        text.textContent = text.textContent.slice(0, -1)
+        counter --;
+    }
+}
+
+function step() {
+    if(counter >= words[currentIndex].length){
+        clearInterval(stepInterval)
+        delTimeout = setTimeout(() => {
+           delIntervalCallback();}, 2000);
+    }
+    else {
+        text.textContent += words[currentIndex][counter];
+        counter ++;
+    }
+}
+
+
+// ----------slidemenu--------
+
+var slidemenu = document.getElementById("slidemenu");
+
+function openmenu (){
+    slidemenu.style.right = "0";
+}
+
+function closemenu (){
+    slidemenu.style.right = "-200px";
+}
+
+//---------to-do--------
+
+
+function getblog(){
+    alert("👾 On the Way Soon.. 🙃");
+}
+
